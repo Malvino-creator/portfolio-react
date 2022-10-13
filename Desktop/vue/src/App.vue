7d5c1a3f6b986d53c3b8fcb3ecd7098a8e03d754
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-13 12:39:03 +0300

    first commit

---
+++ Desktop/vue/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-app-bar app dark color="blue">
+      <v-toolbar-title> Vue Login</v-toolbar-title>
+      <v-btn>Home </v-btn>
+      <v-btn>About</v-btn>
+      <v-btn>Login</v-btn>
+      <v-btn>Contact Us</v-btn>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data()
+   {  
+  return}
+
+}
+</script>
