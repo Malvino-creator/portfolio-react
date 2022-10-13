3643a0d5aa458559f966f501d1654c9ec8b41f93
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-13 16:02:35 +0300

    feat(Navbar): import the logo

--- Desktop/portfolio/src/Components/navbar.js
+++ Desktop/portfolio/src/Components/navbar.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Logo from './logo'
 
 
 function navbar() {
   return (
     <div>
-      
+      <img src={logo} className="App-logo" alt='logo'></img>
     </div>
   )
 }
