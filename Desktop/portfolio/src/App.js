00084df806636df0d22af8e02316e92e95f173e3
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-13 15:22:17 +0300

    feat(Navbar): Create component navbar

--- Desktop/portfolio/src/App.js
+++ Desktop/portfolio/src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Header from './Components/header';
+import navbar from './Components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       
     </div>
   );
