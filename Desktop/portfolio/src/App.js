bc4d22e0be0a7988f97187f70ce53ac9b538cae5
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-14 23:37:18 +0300

    feat(Styles):Import fonts from quicksand

--- Desktop/portfolio/src/App.js
+++ Desktop/portfolio/src/App.js
@@ -1,13 +1,16 @@
-
-import './App.css';
+import React from 'react';
 import Header from './Components/header';
 import Navbar from './Components/Navbar';
+import Home from './Components/home'
+
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <Home />
       
     </div>
   );
