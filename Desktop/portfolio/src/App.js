9535de95a1b1b72bb8ab23bc0a3ec3d1b8b3b331
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-15 20:20:39 +0300

    feat(footer): add footer component

--- Desktop/portfolio/src/App.js
+++ Desktop/portfolio/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import Header from './Components/header';
 import Navbar from './Components/Navbar';
-import Home from './Components/home'
+import Home from './Components/home';
+import Footer from "./Components/footer";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Navbar />
-      <Home />
-      
+   <div className='content'>
+    <Home />
+   </div>
+   <div>
+    <Footer />
+   </div>
     </div>
   );
 }
---
+++ Desktop/portfolio/src/Components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer= () => {
+  return (
+    <div>
+      <h3> Connect now!</h3>
+    </div>
+  )
+}
+
+export default footer;
