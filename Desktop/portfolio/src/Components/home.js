d2cceb56de75da649e8455b790bf27227785203f
Moringa School <malvin.opondo@student.moringaschool.com>
2022-10-18 12:15:37 +0300

    feat(styles): Add image in the assets folder

--- Desktop/portfolio/src/Components/home.js
+++ Desktop/portfolio/src/Components/home.js
@@ -7,9 +7,9 @@ const Home = () =>{
   return (
     <div>
       <h1>
-        Hi,<br>I'm Malvin,</br>
-        a Fullstack Software Engineer. 
-        <button className='check'>Check my Work! </button>
+        Hello, I'm Malvin Opondo,
+        <p>A Fullstack Software Engineer. </p>
+        <button className='btn'>Check my Work! </button>
       </h1>
     </div>
   )
